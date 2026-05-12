4062375ea7c4fabd89b3794e05878be442993bf7
Astha2805 <asthamohanty82@gmail.com>
2026-05-12 15:39:07 +0000

    04_comparision.js file

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==2);
+console.log(2!=1); 
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+//the reason is that an equality check == and comparisions >,<,>=,<= work differently
+//comparisions convert null into 0, >=0 is true and (1) null>0 is false.
+
+// ===
+console.log("2"===2);
